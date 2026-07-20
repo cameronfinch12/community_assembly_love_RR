64d9a87216d3f71a7fbfcaab6ab46bdb6d83505c
Ria Raut <riaraut7@gmail.com>
2026-07-20 13:58:00 -0700

    Made changes to the amphibians processing R script + re-wrote the csv itself! Cleaner desirability columns this way

--- data/amphibian_parasites/Johnson_et_al_data_processing.R
+++ data/amphibian_parasites/Johnson_et_al_data_processing.R
@@ -3,39 +3,43 @@
 
 library(tidyverse)
 
-setwd('C:/Users/riara/OneDrive/All Documents/UCBerk Personal research work/LOVE/LOVE_git_scripts/new_data/Johnson_et_al_2019_royal_society_data')
+setwd('C:/Users/riara/OneDrive/All Documents/UCBerk Personal research work/LOVE/community_assembly_love_RR-NEW')
+getwd()
 
-infections_data <- read.csv('experiment_infection_totals_UPLOAD.csv', stringsAsFactors = F)
+infections_data <- read.csv('data/amphibian_parasites/experiment_infection_totals_UPLOAD.csv', stringsAsFactors = F)
 
 #see if p/a of amphibians (and thus community?) influences p/a of parasite -- not quite the same as persistence 
 #of species across time? 
 #take out host count 
 #Make it so that you're multipling the B, P, etc. host columns by the number of P, B, etc. in the 'treatment' column 
-#these counts are your action columns 
+#these counts are your outcome columns. Not action in this case because action and outcomes are expected to be the same
+#and since this is primarily for function g, let's just call it outcome 
 #then the outcome cols are just...the Count? Change that title to parasite_count 
 
 cleaned <- infections_data %>% 
-  select(-c(HostCount, CommFreq, B, P, R, G, T))
-
+  select(-c(HostCount, CommFreq, B, P, R, G, T, TotalAdded))
+summary(cleaned)
 
 #Make individual counts 
-cleaned$B.action <- str_count(cleaned$Treatment, 'B')
-cleaned$P.action <- str_count(cleaned$Treatment, 'P')
-cleaned$R.action <- str_count(cleaned$Treatment, 'R')
-cleaned$G.action <- str_count(cleaned$Treatment, 'G')
-cleaned$T.action <- str_count(cleaned$Treatment, 'T')
-cleaned$row_ID <- row_number(cleaned)
+cleaned$B.outcome <- str_count(cleaned$Treatment, 'B')
+cleaned$P.outcome <- str_count(cleaned$Treatment, 'P')
+cleaned$R.outcome <- str_count(cleaned$Treatment, 'R')
+cleaned$G.outcome <- str_count(cleaned$Treatment, 'G')
+cleaned$T.outcome <- str_count(cleaned$Treatment, 'T')
+#cleaned$row_ID <- row_number(cleaned)
 
 wider_cleaned <- cleaned %>% 
-  pivot_wider(names_from = Parasite, values_from = Count) %>% 
-  rename(Carriers_added = TotalAdded)
+  pivot_wider(names_from = Parasite, values_from = Count) 
 
-final <- wider_cleaned %>% 
-  select( c (row_ID, 
-             Treatment, 
+expanded <- wider_cleaned %>%
+  unnest(c(Alaria, Cephalogonimus, Ribeiroia))
+
+class(expanded$Alaria) # it says it's an integer, which I think is fine... ?? 
+
+final <- expanded %>% 
+  select( c (Treatment, 
              HostRichness, 
-             Carriers_added,
-             contains('action'), 
+             contains('outcome'), 
              Alaria, 
              Cephalogonimus, 
              Ribeiroia))
